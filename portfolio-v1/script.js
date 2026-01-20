90612dc4fad63147d97b5c00179ce95fcf20205d
samy-belazzoug <samy.belazzoug@laplateforme.io>
2026-01-20 10:07:16 +0100

    Word animation working nicely

---
+++ portfolio-v1/script.js
@@ -0,0 +1,63 @@
+function createFloatingElement(text) {
+    // CREATE AND ANIMATE A FLOATTING ELEMENT
+    const div = document.createElement('div');
+    div.className = 'floating-text';
+    div.textContent = text;
+    div.style.fontFamily = 'var(--font2)'
+    
+    // RANDOM VERTICAL POSITION (between 0-header height - 50px)
+    const randomTop = Math.random() * (document.querySelector('.header').offsetHeight - 50);
+    div.style.top = `${randomTop}px`;
+    
+    // RANDOM DURATION (2-4 seconds)
+    const randomDuration = 2 + Math.random() * 2;
+    div.style.animationDuration = `${randomDuration}s`;
+    
+    document.querySelector('.header').appendChild(div);
+    
+    // GET RID OF THE ELEMENT AFTER ANIMATION
+    setTimeout(() => {
+        div.remove();
+    }, randomDuration * 1000);
+}
+
+// WORD LIST
+const skills = [
+    'Hello, World!',
+    'Python',
+    'sys.exit()',
+    '[x for x in l if x % 2 == 0]',
+    'import sys',
+    'C++',
+    'vector<string> planes',
+    'class Car {}',
+    'C',
+    'gcc -Wall -Werror test.c -o test',
+    '0x178392',
+    'free(&a)',
+    'return 0;',
+    'C#',
+    'Rust',
+    'cargo run',
+    'Unity',
+    'BoxCollider2D',
+    'version control',
+    'Git',
+    'git add Car.cpp',
+    'git checkout -b',
+    'shell linux',
+    'ls -la',
+    'cd ..',
+    'env',
+    'man execve()'
+];
+
+// SPAWN A RANDOM ELEMENT EVERY 2 SECONDS
+setInterval(() => {
+    const randomSkill = skills[Math.floor(Math.random() * skills.length)];
+    createFloatingElement(randomSkill);
+}, 2000);
+
+// SPANW 2 ELEMENTS ON THE LOADING
+createFloatingElement('Hello, World!');
+setTimeout(() => createFloatingElement('0x178392'), 500);
